99791deebe8e87e93fc988d61d1e27cceea12353
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-05-17 07:12:48 +0700

    [docs] include only User and System parts of the API docs

--- pkg/swagger/swagger.go
+++ pkg/swagger/swagger.go
@@ -2,7 +2,7 @@ package swagger
 
 import "embed"
 
-//go:generate swag init --parseDependency --outputTypes json,yaml -d ../../ -g ./cmd/sms-gateway/main.go -o ../../pkg/swagger/docs
+//go:generate swag init --parseDependency --tags=User,System --outputTypes json,yaml -d ../../ -g ./cmd/sms-gateway/main.go -o ../../pkg/swagger/docs
 
 //go:embed docs/*.png docs/*.js docs/*.css docs/swagger.* docs/*.html
 var Docs embed.FS
